f9d3b157a42b05b206ccffe776dd84b8b63eb41a
divhuntcompany <84196532+divhuntcompany@users.noreply.github.com>
2023-09-10 01:38:51 +0200

    Update example_text_completion.py

--- example_text_completion.py
+++ example_text_completion.py
@@ -14,6 +14,7 @@ def main(
     max_seq_len: int = 128,
     max_gen_len: int = 64,
     max_batch_size: int = 4,
+    question: str = "What is no-code?"
 ):
     """
     Entry point of the program for generating text using a pretrained model.
@@ -36,23 +37,7 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    prompts: List[str] = [
-        # For these prompts, the expected answer is the natural continuation of the prompt
-        "I believe the meaning of life is",
-        "Simply put, the theory of relativity states that ",
-        """A brief message congratulating the team on the launch:
-
-        Hi everyone,
-        
-        I just """,
-        # Few shot prompt (providing a few examples before asking model to complete more);
-        """Translate English to French:
-        
-        sea otter => loutre de mer
-        peppermint => menthe poivrée
-        plush girafe => girafe peluche
-        cheese =>""",
-    ]
+    prompts: List[str] = [question]
     results = generator.text_completion(
         prompts,
         max_gen_len=max_gen_len,
